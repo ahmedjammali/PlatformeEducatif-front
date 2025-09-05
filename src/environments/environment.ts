c88eb0498e926eba13c93177826d81941a0146d0
ahmedjammali <ahmedjammali2002@gmail.com>
2025-09-05 12:57:41 +0100

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
-// export const environment = {
-//   production: false,
-//   apiUrl: 'http://localhost:5000/api'
-// };
-
 export const environment = {
   production: false,
-  apiUrl: 'https://api.adminstrationonsschool.com/api'
+  apiUrl: 'http://localhost:5000/api'
 };
+
+// export const environment = {
+//   production: false,
+//   apiUrl: 'https://api.adminstrationonsschool.com/api'
+// };
